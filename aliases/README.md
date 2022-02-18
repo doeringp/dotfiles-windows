c2989d55ce5bba470757cf28a9d6294aa6ecadc5
doeringp <20394732+doeringp@users.noreply.github.com>
2022-02-18 12:43:15 +0100

    Removed no longer useful doskey aliases for cmd.exe

--- aliases/README.md
+++
@@ -1,3 +0,0 @@
-# Aliases for cmd
-
-For how to use doskeys see https://stackoverflow.com/a/21040825/7135763
--- aliases/aliases.cmd
+++
@@ -1,5 +0,0 @@
-@echo off
-
-DOSKEY podman=wsl sudo podman $*
-DOSKEY docker=wsl sudo podman $*
-DOSKEY docker-compose=wsl sudo docker-compose $*
